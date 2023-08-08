02396a173c1330f0b4bfb4cec6ad4376b6a8ba7d
wangzhanf <wangzhanf@126.com>
2023-08-08 11:41:13 +0800

    20230808-02

---
+++ 代码/day20230808/demo.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int igLoops = 99;
+
+class Person{
+    public:
+        void setAge(int a_age){
+            m_age = a_age;
+        }
+    private:
+        int m_age;
+}
+
+struct Student{
+    //
+}student;
+
+typedef struct S_Student{
+    //
+}Student;
+
+void testStruct(Student abc)
+{
+
+}
+
+//枚举的使用
+typedef enum tagClothSizeEnum{
+    S/*0*/,
+    M,
+    XL/*2*/,
+    XXXXL = 6,
+    XXXXXXXXXL/*7*/
+}SIZE;
+
+int main(int argc,char* argv[])
+{
+
+    return 0;
+}
+
+double myAverage(int arg1, 
+                 int arg2,
+                 float arg3.........)
+{
+    if(testCondition.isEmpty && 
+       testCondition.isNotNull){
+        int* ptr = null;
+        for(int i = 0;i < igLoops;i++){
+
+        }
+
+    }
+}
